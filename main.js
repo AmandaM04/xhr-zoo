1058bdd05d47c07780159ca6ccc412e7d6a96250
Amanda Mitchell <wreedamitchell@gmail.com>
2018-03-27 18:11:55 -0500

    added XHR

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log();
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+  };
+  
+  const buildDomString = (animalArray) => {
+    console.log("animalArray", animalArray);
+    let domString = "";
+    animalArray.forEach((animal) => {
+      domString += `<h1>${animalArray[i].name}</h1>`;
+    })
+    printToDom(domString, 'animal-holder')
+  };
+  
+  function executeThisCodeAfterFileLoaded (){
+    console.log("executeThisCodeAfterFileLoaded", executeThisCodeAfterFileLoaded);
+    console.log("this", this);
+    console.log("this.responseText", this.responseText);
+    const data = JSON.parse(this.responseText);
+    console.log("data", data);
+    domString(data.animals);
+  }
+function executeThisCodeIfXHRFails(){
+    console.log("something went wrong");
+  }
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+    myRequest.open("GET", "animals.json");
+    myRequest.send();
+    console.log("myrequest", myRequest);
+  };
+  
+  startApplication();
+  
